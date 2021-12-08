59c755f44fc575a4436ab3f4d0e0d6d4776e4191
Alexander Nestorov <jak.racer@gmail.com>
2021-12-08 16:48:11 +0200

    Order confirmation WIP and order-product component added.

--- pc-point/src/app/order/order-confirm/order-confirm.component.ts
+++ pc-point/src/app/order/order-confirm/order-confirm.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {CartService} from "../../services/user/cart.service";
+import {ProductService} from "../../services/product/product.service";
 
 @Component({
   selector: 'app-order-confirm',
@@ -7,9 +9,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderConfirmComponent implements OnInit {
 
-  constructor() { }
+  total: number = 0;
+  productsInCart: any[] = [];
+  productIds: any[];
+  productsInOrder: any[] = [];
+
+  panelOpenState = true;
+
+  constructor(private cart: CartService, private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.productIds = this.cart.getProducts();
+
+    console.log(this.productIds);
+
+    this.productIds.forEach(id => {
+        this.productService.findProductById(id).subscribe(product => {
+          this.productsInCart.push(product);
+          this.total += product.price;
+          this.productsInOrder = this.findOcc(this.productsInCart, 'name');
+
+          console.log(this.productsInOrder);
+
+        });
+      }
+
+  )
+    console.log(this.productsInCart);
+
+
+
+  }
+
+   findOcc(arr, key): any[]{
+    let arr2 = [];
+
+    arr.forEach((x)=>{
+
+      // Checking if there is any object in arr2
+      // which contains the key value
+      if(arr2.some((val)=>{ return val[key] == x[key] })){
+
+        // If yes! then increase the occurrence by 1
+        arr2.forEach((k)=>{
+          if(k[key] === x[key]){
+            k["occurrence"]++
+          }
+        })
+
+      }else{
+        // If not! Then create a new object initialize
+        // it with the present iteration key's value and
+        // set the occurrence to 1
+        let a = {}
+        a[key] = x[key]
+        a["occurrence"] = 1
+        arr2.push(a);
+      }
+    })
+     return arr2
   }
 
 }
--- pc-point/src/app/order/order.module.ts
+++ pc-point/src/app/order/order.module.ts
@@ -3,16 +3,24 @@ import { CommonModule } from '@angular/common';
 import {AuthActivate} from "../guards/auth.activate";
 import { OrderItemComponent } from './order-item/order-item.component';
 import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
+import {CommonsModule} from "../commons/commons.module";
+import {MatExpansionModule} from "angular/material/expansion";
+import {MatButtonModule} from "angular/material/button";
+import { OrderProductComponent } from './order-product/order-product.component';
 
 
 
 @NgModule({
   declarations: [
     OrderItemComponent,
-    OrderConfirmComponent
+    OrderConfirmComponent,
+    OrderProductComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    CommonsModule,
+    MatExpansionModule,
+    MatButtonModule
   ],
   exports: [
     OrderItemComponent
